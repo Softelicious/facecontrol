752f648c7efafa82895f2e962be93ff264e04523
Softelicious <dominykas.rakunas@gmail.com>
2019-03-16 14:07:13 +0200

    ss

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -1,23 +1,25 @@
 @extends('master')
 @section('content')
-<nav class="navigacija navbar flex-row">
-    <ul class="navbar navbar-brand flex-row mr-lg-0">
-        <a class="login btn btn-item "href="login">Login</a>
-        <a class="reg btn btn-item btn-dark" href="register">Register</a>
-    </ul>
-    <div id="init" class="init justify-content-end text-white" >{{$data['username']}}</div>
-</nav>
-<div class="container p-0">
-    <div class="camera rounded bg-dark text-white "><div id="landing" class="flex row align-content-center justify-content-center w-75">
-            @if($data['username'] == 'Svečias')
-                <p>Sveiki atvykę į "Face control" projektą</p>
-                <p>Tikslas - atpažinti nepageidaujamus žmones pagal jų veidus. Aplikacija lange transliuos kameros vaizdą. Nufotografuotas vaizdas bus palyginamas su nepageidaujamais asmenimis.</p>
-                <a href="#" onclick="landing()" class="btn btn-primary btn-success"><span class="glyphicon glyphicon-hand-right">
-                    </span> Pirmyn</a></div><img id="loading" class="loading" src="images/load.gif"/></a>
-            @endif
-            @if($data['username'] != 'Svečias')
-                <p>bus kamera, </p>
-            @endif
+    <nav class="navigacija navbar flex-row">
+        <ul class="navbar navbar-brand flex-row mr-lg-0">
+            <a class="login btn btn-item "href="login">Login</a>
+            <a class="reg btn btn-item btn-dark" href="register">Register</a>
+        </ul>
+        <div id="init" class="init justify-content-center text-white" >Naktinis klubas: <i>neparduota</i></div>
+        <div id="init" class="init justify-content-end text-white" >Apsauginis: Foxtrot</div>
+    </nav>
+    <div class="container p-0">
+        <div class="camera rounded bg-dark text-white ">
+            <div id="landing" class="flex row align-content-center justify-content-center w-75">
+                <video id="vid" >video</video>
+                    <a href="{{URL::to('/check/blacklist')}}" type="button" class="photo-check btn btn-primary mb-3" value="">Tikrinti</a>
+                    <div class = "control-panel">
+                        <ul class="photo d-flex flex-row bd-highlight">
+                            <a href="{{URL::to('/add_to/blacklist')}}" type="button" class="p-2 bd-highlight btn btn-danger mr-3" value="">Į juodąjį sąrašą</a><br>
+                            <a href="{{URL::to('/delete_from/blacklist')}}" type="button" class="p-2 bd-highlight btn btn-success mr-3" value="">Iš juodojo sąrašo</a>
+                        </ul>
+                    </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
