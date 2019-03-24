04039de8f81023cd7b5dfd0ef5366e5669f6133e
Softelicious <dominykas.rakunas@gmail.com>
2019-03-24 19:43:05 +0200

    created UI full

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,49 +13,5 @@ use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
-    public function register(Request $request)
-    {
-        $this->validate($request, [
-            'username' => 'required|unique:users',
-            'password' => 'required|confirmed',
-            'email' => 'required',
-            'user_image' => 'image|required'
-        ]);
 
-        $fileNameAndExtention = $request->file('user_image')->getClientOriginalName();
-        $fileName = pathinfo($fileNameAndExtention, PATHINFO_FILENAME);
-        $fileExtention = $request->file('user_image')->getClientOriginalExtension();
-        $fileNameToStore = $fileName."_".time().".".$fileExtention;
-        $path = $request->file('user_image')->storeAs('public/users_images', $fileNameToStore);
-
-        $user = new User();
-        $user->username = $request['username'];
-        $user->password = Hash::make($request['password']);
-        $user->email= $request['email'];
-        $user->user_image= $fileNameToStore;
-        $user->save();
-        return redirect('/login');
-    }
-
-    public function login(Request $request){
-        $this->validate($request, [
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-        $username = $request['username'];
-        $pass = $request['password'];
-        if(Hash::check($pass, DB::table('users')->where('username', $username)->value('password'))){
-            $data = [
-                'username' => 'Vyr. Apsauginis: ' . $username,
-                'filePathSym' => null,
-                'filePath' => null,
-                'fileName' => null,
-                'message'=> "Spausk 'Tikrinti', kad sužinotum ar praleisti lankytoją"
-            ];
-
-            return view('layouts.content')->with('data', $data);
-        }else{
-            return redirect('/login');
-        }
-    }
 }
